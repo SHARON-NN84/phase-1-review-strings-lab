ffab9d9dac950492053ec0ce873e4d029cea15fa
sharon <njerisharon84@gmail.com>
2025-06-12 16:04:24 +0300

    sring-labs

---
+++ dom.js
@@ -0,0 +1,15 @@
+let sadEmotionContainer = document.
+getElementById("sad")
+
+console.log(sadEmotionContainer)
+
+
+let outputcontainer = document .
+getElementById ("output")
+console.log(outputContainer)
+
+happy emotionalconatiner.
+addEventlistener("click ,()")=> (
+    
+)
+
